348456d9bb6c017e812972e4dffa61ffb467a9e1
Your Name~ <timrogersj@gmail.com>
2022-09-19 10:13:04 -0500

    recent changes

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container, Row, Col, ListGroup, Image } from 'react-bootstrap';
+
+export const homePage=()=>{
+    // before login
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <ListGroup>
+                        <ListGroup.Item>Welcome to Grow</ListGroup.Item>
+                        <ListGroup.Item>The one place you need to keep track of your garden</ListGroup.Item>
+                        <ListGroup.Item>We all have goals</ListGroup.Item>
+                        <ListGroup.Item>Let us get there together</ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col>
+                    <Image></Image>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
