41f6f6520375c11b14f6a50f91d3d6d203b30a74
Shawn <lcorel86@gmail.com>
2022-09-19 21:19:23 -0500

    save before checkout

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import NotificationPage from "./pages/notification/Notification.page";
 
 import ProfileNavbar from "./components/navbar/profile-navbar";
 import ProfilePage from "./pages/profilePage/ProfilePage"
-import LogInPage from "./login/LogInPage";
-import CropperTester from "./components/CropperTester";
+import LogInPage from "./LogIn/LogInPage"
+// import CropperTester from "./components/CropperTester";
 import MainNavbar from "./components/navbar/main-navbar";
 import { HomePage } from "./pages/homepage/homepage";
 import MainBody from "./components/main_body/main_body";
