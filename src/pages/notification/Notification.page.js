dacd8df74cbfda980b7951722a72bf058556d2fb
JasmineJeyasingh <jasmine.a.jeyasingh@gmail.com>
2022-09-22 12:06:36 -0500

    updated notification module styles

--- src/pages/notification/Notification.page.js
+++ src/pages/notification/Notification.page.js
@@ -7,25 +7,21 @@ import Tomorrow from "../../components/notification/Tomorrow";
 
 export default function NotificationPage() {
   return (
-    <>
-      <div className={classes.box}>
-        <p className={classes.header}>Water Schedule</p>
-        <div>
-          <div className={classes.notificationpage}>
-            {/* <Header /> */}
-            <div>
-              <div>
-                <h1 className={classes.h1}>Today</h1>
-                <Today />
-              </div>
-              <div>
-                <h1 className={classes.h1}>Tomorrow</h1>
-                <Tomorrow />
-              </div>
-            </div>
+    <div className={classes.box}>
+      <p className={classes.header}>
+        Water Schedule
+        <div className={classes.notificationpage}>
+          {/* <Header /> */}
+          <div>
+            <h1 className={classes.h1}>Today</h1>
+            <Today />
+          </div>
+          <div>
+            <h1 className={classes.h1}>Tomorrow</h1>
+            <Tomorrow />
           </div>
         </div>
-      </div>
-    </>
+      </p>
+    </div>
   );
 }
